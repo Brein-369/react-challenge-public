1b699acebeb27e8473be5836dd8d3dd86a33889c
Brein-369 <edwardbrainerd@gmail.com>
2021-03-30 11:14:48 +0700

    ubah class ke functional component + using use effect

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -5,99 +5,82 @@ import React from 'react'
 import SuperheroCard from './components/SuperheroCard'
 import AddForm from './components/AddForm'
 import Navbar from './components/Navbar'
+import {useState} from 'react'
+import {useEffect} from 'react'
 
-class App extends React.Component {
-  constructor(props){
-    super(props)
 
-    this.state= {
-      title: "Multiple Universes Superheroes",
-      filteredHero:{},
-      superheroLists : []
-      
-    }
-  }
 
-  addHero = (superhero) =>{
-    let newData = this.state.superheroLists.concat(superhero)
-    this.setState({
-      ...this.state,
-      superheroLists : newData
-    })
-  }
 
-  getSeveralHeroes = () =>{
+
+function App() {
+  
+  const [title, setTitle] = useState('Multiple Universes Superheroes')
+  const [superheroLists, setSuperheroLists] = useState([])
+  const [filteredHero, setFilteredHero] = useState({})
+  
+  useEffect(() => {
     fetch(`https://akabab.github.io/superhero-api/api/all.json`)
     .then(res=>{
       return res.json()
     })
     .then(res=>{
       let severalHeroes = res.slice(0,5)
-      this.setState({
-        ...this.state,
-        superheroLists : severalHeroes
-      })
+      setSuperheroLists(severalHeroes)
     })
     .catch(err=>{
       console.log(err);
     })
-  }
+    // CLEAN UP ??
+    // return () => {
+      //   cleanup
+      // }
+    }, [])
 
-  filterHero = (searchName) => {
-    console.log(searchName,'searchname di filter hero app.js');
-    let filtered = this.state.superheroLists.filter((hero)=>{
-      return hero.name.toLowerCase() === searchName.toLowerCase()
-    })
-    this.setState({
-      ...this.state,
-      filteredHero: filtered
-    })
-    console.log(this.state.filteredHero,'<<<<<<');
-  }
-
-  // componentDidUpdate(){
-
-  // }
-
-  componentDidMount(){
-    this.getSeveralHeroes()
-  }
-
-  render() {
-    // const {users} = this.users
-    if (!this.state.filteredHero.length){
-      return (
-        // harus satu lead tag ( sama seperti vue )
-        <div>
-          <Navbar filterHero={this.filterHero}></Navbar>
-          <h1 className="text-center my-5">{this.state.title}</h1>
-          <div className="text-center my-5">
-            <h3>Add Superhero</h3>
-            <AddForm addSuperhero={this.addHero}></AddForm>
-          </div>
-          <div className="d-flex row justify-content-center mx-5">
-            {
-              this.state.superheroLists.map(superhero=>{
-                return <SuperheroCard superhero={superhero} key={superhero.id}></SuperheroCard>
-              })
-            }
-          </div>
-        </div>
-      )
+    function filterHero (searchName) {
+      console.log(searchName,'searchname di filter hero app.js');
+      let filtered = superheroLists.filter((hero)=>{
+        return hero.name.toLowerCase() === searchName.toLowerCase()
+      })
+      setFilteredHero(filtered)
+      console.log(filtered,'<<<<<<');
     }
-    else {
-      return (
-        // harus satu lead tag ( sama seperti vue )
-        <div>
-          <Navbar filterHero={this.filterHero}></Navbar>
-          <h1 className="text-center my-5">{this.state.title}</h1>
-          <div className="d-flex row justify-content-center mx-5">
-            <SuperheroCard superhero={this.state.filteredHero[0]} key={this.state.filteredHero[0].id}></SuperheroCard>
-          </div>
+
+    function addHero(superhero){
+        setSuperheroLists(superheroLists.concat(superhero))
+      }
+      
+      if (!filteredHero.length){
+        return (
+      // harus satu lead tag ( sama seperti vue )
+      <div>
+        <Navbar filterHero={filterHero}></Navbar>
+        <h1 className="text-center my-5">{title}</h1>
+        <div className="text-center my-5">
+          <h3>Add Superhero</h3>
+          <AddForm addSuperhero={addHero}></AddForm>
         </div>
-      )
-    }
-  }  
+        <div className="d-flex row justify-content-center mx-5">
+          {
+            superheroLists.map(superhero=>{
+              return <SuperheroCard superhero={superhero} key={superhero.id}></SuperheroCard>
+            })
+          }
+        </div>
+      </div>
+    )
+  }
+  else {
+    return (
+      // harus satu lead tag ( sama seperti vue )
+      <div>
+        <Navbar filterHero={filterHero}></Navbar>
+        <h1 className="text-center my-5">{title}</h1>
+        <div className="d-flex row justify-content-center mx-5">
+          <SuperheroCard superhero={filteredHero[0]} key={filteredHero[0].id}></SuperheroCard>
+        </div>
+      </div>
+    )
+  }
 }
 
 
